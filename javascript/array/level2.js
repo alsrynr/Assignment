1c275d10fe66e0fdacf91cb9cf5bb02439491842
unknown <alisha.rauniyarals@gmail.com>
2024-06-04 17:50:56 +0545

    level 2

---
+++ javascript/array/level2.js
@@ -0,0 +1,19 @@
+const arr = ['kaylin', 'ram', 'gopal', 'ankit']
+
+const userDetails = {
+    'kaylin': 'khanal',
+    'alisha': 'rauniyar',
+    'ayush': 'rai',
+    'gopal': 'tharu'
+}
+
+arr.map((item)=>{
+    if (userDetails[item]){
+      return item +' '+ userDetails[item]
+    }
+   }).filter(item=>item)
+
+
+
+// expected output :
+// ['kaylin khanal', 'gopal tharu']
